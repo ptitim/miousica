02f83967803be21e4042214ea4424d857afc1710
nope nope <nope nope>
2016-09-18 12:07:32 +0200

    First commit

---
+++ script.ts
@@ -0,0 +1,15 @@
+var red:number = 0;
+var green:number = 0;
+var blue:number = 0;
+
+var previousPositionX:number = 0;
+var previousPositionY:number = 0;
+
+function test(event){
+    var mousex = event.clientX;
+    var mousey = event.clientY;
+
+    var deltax = Math.abs(mouseX - previousPositionX);
+    var deltay = Math.abs(mouseY - previousPositionY);
+
+}
