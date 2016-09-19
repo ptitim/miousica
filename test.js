5b3ba256c3783a860f5a5d21d03ded1aaa551982
ptitim <timgamer@hotmail.fr>
2016-09-19 17:32:42 +0200

    bordure

--- test.js
+++ test.js
@@ -6,7 +6,16 @@ var keyTab = [{note:'C3', freq:261, key:'w',  keyCode:87,  press:false, osc: und
               {note:'A3', freq:440, key:'n',  keyCode:78,  press:false, osc: undefined, div: undefined},
               {note:'B3', freq:494, key:',',  keyCode:188, press:false, osc: undefined, div: undefined},
               {note:'C4', freq:523, key:';',  keyCode:59,  press:false, osc: undefined, div: undefined},
-              {note:'D4', freq:587, key:':',  keyCode:58,  press:false, osc: undefined, div: undefined}];
+              {note:'D4', freq:587, key:':',  keyCode:58,  press:false, osc: undefined, div: undefined},
+              {note:'C3#', freq:277, key:'s',  keyCode:83,  press:false, osc: undefined, div: undefined},
+              {note:'D3#', freq:311, key:'d',  keyCode:68,  press:false, osc: undefined, div: undefined},
+              {note:'F3#', freq:370, key:'g',  keyCode:71,  press:false, osc: undefined, div: undefined},
+              {note:'G3#', freq:415, key:'h',  keyCode:72,  press:false, osc: undefined, div: undefined},
+              {note:'A3#', freq:466, key:'j',  keyCode:74,  press:false, osc: undefined, div: undefined},
+              {note:'C4#', freq:554, key:'l',  keyCode:76,  press:false, osc: undefined, div: undefined},
+              {note:'D4#', freq:622, key:'m',  keyCode:77,  press:false, osc: undefined, div: undefined},
+
+              ];
 
 function showme(event){
     // console.log(event);
@@ -67,8 +76,8 @@ function playNote(note){
     osc.detune.value = -10;
 
 
-    osca.type = 'triangle';
-    osca.frequency.value = note.freq;
+    osca.type = 'sine';
+    osca.frequency.value = note.freq + 2;
     osca.detune = -5;
 
     osc2.type = 'sine';
@@ -96,12 +105,12 @@ function createDivNote(note){
     var div = document.createElement('div');
     var rgb = "rgb(" + rand(255).toString() + "," + rand(255) + "," + rand(255)+ ")";
     // div.style.backgroundColor = rgb;
-    div.style.border = "3px solid " + rgb;
+    div.style.border = "5px ridge " + rgb;
     div.style.borderRadius = "100%";
     div.style.position = "absolute";
     div.style.top = randBetween(10,20).toString() + '%';
-    div.style.height = "4vh";
-    div.style.width = "4vh";
+    div.style.height = "8vh";
+    div.style.width = "8vh";
     div.className += "anim"
 
     if(note.note == "C3"){
@@ -122,7 +131,22 @@ function createDivNote(note){
       div.style.left = randBetween(10,80).toString() + "%";
     }else if (note.note == "D4") {
       div.style.left = randBetween(8,90).toString() + "%";
+    }else if (note.note == "C3#") {
+      div.style.left = randBetween(5,10).toString() + "%";
+    }else if (note.note == "D3#") {
+      div.style.left = randBetween(5,20).toString() + "%";
+    }else if (note.note == "F3#") {
+      div.style.left = randBetween(5,30).toString() + "%";
+    }else if (note.note == "G3#") {
+      div.style.left = randBetween(5,40).toString() + "%";
+    }else if (note.note == "A3#") {
+      div.style.left = randBetween(5,50).toString() + "%";
+    }else if (note.note == "C4#") {
+      div.style.left = randBetween(5,80).toString() + "%";
+    }else if (note.note == "D4#") {
+      div.style.left = randBetween(5,90).toString() + "%";
     }
+
     var parentElement = document.getElementsByClassName('principal')[0];
     parentElement.appendChild(div);
     return div;
