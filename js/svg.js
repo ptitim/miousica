495e0017339a8d2fcff5832b232617963455f1d6
ptitim <timgamer@hotmail.fr>
2016-09-29 17:27:06 +0200

    ajout de la librairie svg.js, animation et generation a travailler

---
+++ js/svg.js
@@ -0,0 +1,6 @@
+function svgTest() {
+    var princ = document.getElementsByClassName('principal')[0];
+    var svg = SVG(princ).size(1000, 1000);
+    var image = svg.image('data/img/blue-154328.svg');
+    image.size(100, 100);
+}
